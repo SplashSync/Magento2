965218e6ed59fcaf187104837e982fe60d8465ad
BadPixxel <eshop.bpaquier@gmail.com>
2021-06-01 18:26:39 +0200

    WIP

--- docker/magento/scripts/install-sampledata.sh
+++ docker/magento/scripts/install-sampledata.sh
@@ -1,9 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
+################################################################################
+#
+#  This file is part of SplashSync Project.
+#
+#  Copyright (C) Splash Sync <www.splashsync.com>
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  For the full copyright and license information, please view the LICENSE
+#  file that was distributed with this source code.
+#
+#  @author Bernard Paquier <contact@splashsync.com>
+#
+################################################################################
 
-cd /var/www/html
-su www-data
+cd /var/www
+#su www-data
 
-cp $COMPOSER_HOME/auth.json $INSTALL_DIR/var/composer_home/auth.json
-
-bin/magento sampledata:deploy
+################################################################################
+echo "Clone Sample Data Repository"
+git clone https://github.com/magento/magento2-sample-data.git sampledata $MAGENTO_VERSION
+cd sampledata
+git checkout $MAGENTO_VERSION
+################################################################################
+echo "Deploy Sample Data"
+chown -R www-data:www-data /var/www/sampledata/
+php -f /var/www/sampledata/dev/tools/build-sample-data.php -- --ce-source="/var/www/html"
+chown -R www-data:www-data /var/www/html/
+################################################################################
+# ReCompile Magento
 compile-magento.sh
+
