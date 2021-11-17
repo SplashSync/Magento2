35b7cc5e715f31944bc01503e669a63567c81fdd
BadPixxel <eshop.bpaquier@gmail.com>
2021-11-17 12:48:16 +0100

    WIP: Composer Local Package

--- docker/magento/scripts/create-magento.sh
+++ docker/magento/scripts/create-magento.sh
@@ -7,7 +7,7 @@ echo "Create Project"
 su www-data -c "composer create-project --no-progress --repository-url=https://repo.magento.com/ magento/project-community-edition $INSTALL_DIR $MAGENTO_VERSION"
 
 echo "Install SplashSync Module via Composer"
-composer config repositories.splash '{ "type": "path", "url": "/builds/SplashSync/Magento2", "options": { "symlink": true } }'
+composer config repositories.splash '{ "type": "path", "url": "/builds/SplashSync/Magento2", "options": { "symlink": true, "versions": { "splash/magento2": "dev-local" }}}'
 composer config minimum-stability dev
 COMPOSER_MEMORY_LIMIT=-1 composer require splash/magento2:dev-master --no-scripts --no-progress --no-suggest
 composer info | grep "splash"
